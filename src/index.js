1e25d680fd11dfbe60583b3dcde79be64dcca7f8
eliasall3x <elliasallex@gmail.com>
2021-05-02 17:22:56 -0300

    feat: pushed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://instagram.com/rocketseat_oficial");
 
