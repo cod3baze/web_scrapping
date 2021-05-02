a8b9ea6fe987b9605bb6f67d723af2d7414c14bb
eliasall3x <elliasallex@gmail.com>
2021-05-02 17:08:49 -0300

    feat: html node

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ pega dados em web sites
 ## puppeteer
 
 baixa um navegador baseado no chrome, para visitar as páginas onde vão ser extraídas os dados necessário.
+
+- `page.evaluate(func)`: **func** vai ser executada dentro da instância do browser.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://rocketseat.com.br");
+  await page.goto("https://instagram.com/rocketseat_oficial");
 
   // Toda essa função vai ser executada no browser
-  await page.evaluate(() => {});
+  const imgList = await page.evaluate(() => {
+    // pegar todas images no posts
+    const nodeList = document.querySelectorAll("article img");
+    // transformar o NodeList em array
+    const imgArray = [...nodeList];
 
-  await browser.close();
+    const list = imgArray.map(({ src }) => ({
+      src,
+    }));
+
+    return list;
+  });
+
+  fs.writeFile("instagram.json", JSON.stringify(imgList, null, 2), (error) => {
+    if (error) throw new Error("Something went wrong");
+
+    console.log("images saved ✔");
+  });
+
+  // await browser.close();
 })();
